2c5ab2af58b09e820cc81750ea65c25896cde277
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-17 14:49:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,29 +1,7 @@
 
-def nyc_pigeon_organizer(data)
-  name_hash = {}
-  data.each do |key, value|
-      value.collect do |key2, value2|
-        value2.collect do |name|
-         if !name_hash[name] 
-           name_hash[name] = {}
-         end
-      if !name_hash[name][key]
-        name_hash[name][key] = []
-        # binding.pry
-      end
-      name_hash[name][key].push(key2.to_s)
-      # binding.pry
-    end
-  end
-end
-
-end
-
-
-
-
 # def nyc_pigeon_organizer(data)
-#   data.each_with_object({}) do |(key, value), name_hash|
+#   name_hash = {}
+#   data.each do |key, value|
 #       value.collect do |key2, value2|
 #         value2.collect do |name|
 #         if !name_hash[name] 
@@ -40,6 +18,28 @@ end
 # end
 
 # end
+
+
+
+
+def nyc_pigeon_organizer(data)
+  data.each_with_object({}) do |(key, value), name_hash|
+      value.collect do |key2, value2|
+        value2.collect do |name|
+        if !name_hash[name] 
+          name_hash[name] = {}
+        end
+      if !name_hash[name][key]
+        name_hash[name][key] = []
+        # binding.pry
+      end
+      name_hash[name][key].push(key2.to_s)
+      # binding.pry
+    end
+  end
+end
+
+end
 
 # push data into an array, then turn array into hash??
 
